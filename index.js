15ce1a85b9794a3b3437a113ddda4cf1409f80e0
Dan Mindru <mindrudan@gmail.com>
2017-07-03 17:21:07 +0200

    New packages and 1st part of the how-to animation

--- index.js
+++ index.js
@@ -1,30 +1,94 @@
-import './style';
-import { Component } from 'preact';
-import { Result } from './result';
+import './style.scss'
+import { Component } from 'preact'
+import { TimelineLite, SlowMo, Power2 } from 'gsap'
+import {
+  FontIcon,
+  ImageIcon,
+  TextIcon,
+  VideoIcon,
+  Document,
+  Headline,
+  Image,
+  Laptop,
+  LaptopContents,
+  Video,
+  Pipe
+} from './svg'
 
-const SEARCH = '//api.github.com/search/repositories';
+const qs = (s) => document.querySelector(s)
 
 export default class App extends Component {
-	componentDidMount() {
-		fetch(`${SEARCH}?q=preact`)
-			.then( r => r.json() )
-			.then( json => {
-				this.setState({
-					results: json && json.items || []
-				});
-			});
-	}
+  componentDidMount () {
+    const iconGroupDOM = qs('#iconGroup')
+    const fontIconDOM = qs('#fontIcon')
+    const imageIconDOM = qs('#imageIcon')
+    const videoIconDOM = qs('#videoIcon')
+    const textIconDOM = qs('#textIcon')
+    const laptopDOM = qs('#laptop')
+    const laptopContentsDOM = qs('#laptopContents')
+    const headlineDOM = qs('#headline')
+    const imageDOM = qs('#image')
+    const documentDOM = qs('#document')
+    const videoDOM = qs('#video')
+    const editorDOM = qs('#editor')
 
-	render(props, { results=[] }) {
-		return (
-			<div>
-				<h1>Example</h1>
-				<div class="list">
-					{ results.map( result => (
-						<Result result={result} />
-					)) }
-				</div>
-			</div>
-		);
-	}
+    const timeline = new TimelineLite()
+
+    timeline
+      .from(textIconDOM, 0.5, { opacity: 0, rotation: -3, scale: 2, ease: Power2.easeIn })
+      .from(fontIconDOM, 0.5, { opacity: 0, rotation: 5, scale: 2, ease: Power2.easeIn })
+      .from(imageIconDOM, 0.5, { opacity: 0, rotation: -5, scale: 2, ease: Power2.easeIn })
+      .from(videoIconDOM, 0.5, { opacity: 0, rotation: 3, scale: 2, ease: Power2.easeIn })
+      .from(laptopDOM, 0.5, { y: 80, opacity: 0, rotation: 0, ease: Power2.easeIn })
+      .to(iconGroupDOM, 0.5, { scale: 0.4, y: 25, x: 350 })
+
+      .to(laptopContentsDOM, 2, { opacity: 1 })
+      .to(fontIconDOM, 0.5, { opacity: 1, rotation: 0, scale: 1, ease: Power2.easeIn, x: 40, y: 30 })
+      .to(textIconDOM, 0.5, { opacity: 1, rotation: 0, scale: 1, ease: Power2.easeIn, x: 80, y: 30 })
+      .to(imageIconDOM, 0.5, { opacity: 1, rotation: 0, scale: 1, ease: Power2.easeIn, x: 120, y: 30 })
+      .to(videoIconDOM, 0.5, { opacity: 1, rotation: 0, scale: 1, ease: Power2.easeIn, x: 160, y: 30 })
+
+      .to(fontIconDOM, 2, { opacity: 0, rotation: 0, scale: 1, ease: SlowMo.easeOut, x: 70, y: 130 }, 'headline')
+      .to(headlineDOM, 1, { opacity: 1, ease: SlowMo.easeOut }, 'headline')
+
+      .to(textIconDOM, 2, { opacity: 0, rotation: 0, scale: 1, ease: SlowMo.easeOut, x: 130, y: 180 }, 'headline+=0.5')
+      .to(documentDOM, 1, { opacity: 1, ease: SlowMo.easeOut }, 'headline+=0.5')
+
+      .to(imageIconDOM, 2, { opacity: 0, rotation: 0, scale: 1, ease: SlowMo.easeOut, x: 50, y: 210 }, 'headline+=1')
+      .to(imageDOM, 1, { opacity: 1, ease: SlowMo.easeOut }, 'headline+=1')
+
+      .to(videoIconDOM, 2, { opacity: 0, rotation: 0, scale: 1, ease: SlowMo.easeOut, x: 230, y: 210 }, 'headline+=1.5')
+      .to(videoDOM, 1, { opacity: 1, ease: SlowMo.easeOut }, 'headline+=1.5')
+
+      .to(editorDOM, 0.5, { x: -200, ease: SlowMo.easeOut })
+
+    timeline.timeScale(4)
+  }
+
+  render () {
+    return (
+      <main>
+        <svg viewBox="0 0 768 300">
+          <g id="editor">
+            <Laptop id="laptop" opacity="1" transform="translate(200, 30), scale(1.5)" />
+            <LaptopContents id="laptopContents" opacity="0" transform="translate(291.5, 31.5), scale(1.5)" />
+
+            <Headline id="headline" opacity="0" transform="translate(335, 80), scale(1.5)" />
+            <Image id="image" opacity="0" transform="translate(335, 105), scale(1.5)" />
+            <Document id="document" opacity="0" transform="translate(390, 105), scale(1.5)" />
+            <Video id="video" opacity="0" transform="translate(430, 67), scale(1.5)" />
+
+            <g id="iconGroup" transform="translate(310, -35), scale(1.3)">
+              <TextIcon id="textIcon" opacity="1" transform="translate(45, 70), rotate(-10), scale(1.5)" />
+              <FontIcon id="fontIcon" opacity="1" transform="translate(30, 10), rotate(5), scale(1.5)" />
+              <ImageIcon id="imageIcon" opacity="1" transform="translate(0, 70), rotate(-15), scale(1.5)" />
+              <VideoIcon id="videoIcon" opacity="1" transform="translate(65, 42), rotate(-15), scale(1.5)" />
+            </g>
+          </g>
+
+          <Pipe transform="translate(310, 50)" customWidth={'200'} />
+        </svg>
+      </main>
+    )
+  }
 }
